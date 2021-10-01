a935f3be37bd6edf1f040351582c6f8a5fec5025
merBec <becic.merjema@gmail.com>
2021-10-01 22:56:47 +0200

    Added ex04

---
+++ ex04/src/index.js
@@ -0,0 +1,18 @@
+import React, {Component} from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import { Component } from "react";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello, Padawans!</h1>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
+
